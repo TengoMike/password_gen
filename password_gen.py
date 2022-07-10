7bc6767d1f3227f98bcfdee8edc30670d8f3d241
TengoMike <71284110+TengoMike@users.noreply.github.com>
2022-07-10 22:07:01 +0200

    update_v3

--- password_gen.py
+++ password_gen.py
@@ -10,7 +10,6 @@ numbers_list = [] #10 elementów
 
 for number in range(0,10):
     numbers_list.append(str(number))
-
 char_list = []
 
 length = input("Ile znaków ma mieć hasło?\n")
@@ -18,66 +17,39 @@ while length.isnumeric() == False:
     print("Musisz podać liczbę!")
     length = input("Ile znaków ma mieć hasło?\n")
 else:
-    option = input("Czy znaki mogą się powtarzać?\n")
-    if option == 'nie' or option == 'n' or option == 'no' or option == 'n':
-
-        lower = input("Czy mają być małe litery?\n")
-        if lower == 'tak' or lower == 't' or lower == 'yes' or lower == 'y':
-            char_list += letter_lower_list
-        else:
-            pass
+    lower = input("Czy mają być małe litery?\n")
+    if lower == 'tak' or lower == 't' or lower == 'yes' or lower == 'y':
+        char_list += letter_lower_list
+    else:
+        pass
     
-        upper = input("Czy mają być duże litery?\n")
-        if upper == 'tak' or upper == 't' or upper == 'yes' or upper == 'y':
-            char_list += letter_upper_list
-        else:
-            pass
+    upper = input("Czy mają być duże litery?\n")
+    if upper == 'tak' or upper == 't' or upper == 'yes' or upper == 'y':
+        char_list += letter_upper_list
+    else:
+        pass
 
-        numbers = input("Czy mają być cyfry?\n")
-        if numbers == 'tak' or numbers == 't' or numbers == 'yes' or numbers == 'y':
-            char_list += numbers_list
-        else:
-            pass
+    numbers = input("Czy mają być cyfry?\n")
+    if numbers == 'tak' or numbers == 't' or numbers == 'yes' or numbers == 'y':
+        char_list += numbers_list
+    else:
+        pass
 
-        special = input("Czy mają być znaki specjalne?\n")
-        if special == 'tak' or special == 't' or special == 'yes' or special == 'y':
-            char_list += special_signs
-        else:
-            pass
+    special = input("Czy mają być znaki specjalne?\n")
+    if special == 'tak' or special == 't' or special == 'yes' or special == 'y':
+        char_list += special_signs
+    else:
+        pass
+    
+    option = input("Czy znaki mogą się powtarzać?\n")
+    if option == 'tak' or option == 't' or option == 'yes' or option == 'y':
+            password = "".join(random.choices(char_list, k = int(length))) #generuje hasła z możliwością powtarzania znaków
+            print(f"Twoje hasło to: {password}")
+    else:
         if int(length) > len(char_list):
             print("Hasło jest za długie względem ilości wybranych znaków!")
             len = int(len(char_list))+1
             print(f"Jeśli chcesz kontynuować podaj wartość mniejszą niż {len}")
         else:
-            password = "".join(random.sample(char_list, int(length))) 
-        # to generuje hasło w którym nie powtarzają się znaki i nie ma pewności, że np. wszystkie znaki wystąpią
-        # dla hasła o długości 2 mogą wystąpić tylko małe litery 
-        # tak samo dla hasła o długości 10 mogą wystapić same litery 
+            password = "".join(random.sample(char_list, int(length))) #generuje hasło bez powtarzania znaków 
             print(f"Twoje hasło to: {password}")
-    else:
-        lower = input("Czy mają być małe litery?\n")
-        if lower == 'tak' or lower == 't' or lower == 'yes' or lower == 'y':
-            char_list += letter_lower_list
-        else:
-            pass
-    
-        upper = input("Czy mają być duże litery?\n")
-        if upper == 'tak' or upper == 't' or upper == 'yes' or upper == 'y':
-            char_list += letter_upper_list
-        else:
-            pass
-
-        numbers = input("Czy mają być cyfry?\n")
-        if numbers == 'tak' or numbers == 't' or numbers == 'yes' or numbers == 'y':
-            char_list += numbers_list
-        else:
-            pass
-
-        special = input("Czy mają być znaki specjalne?\n")
-        if special == 'tak' or special == 't' or special == 'yes' or special == 'y':
-            char_list += special_signs
-        else:
-            pass
-
-        password = "".join(random.choices(char_list, k = int(length))) 
-        print(f"Twoje hasło to: {password}")
